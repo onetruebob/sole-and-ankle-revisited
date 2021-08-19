1918d81f9f185997c0dd58acbb0f4dc28606be03
Bob Owen <bob@bobowen.org>
2021-08-19 19:20:30 -0400

    Completed exercise

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -21,15 +23,27 @@ const Header = () => {
         <Side>
           <Logo />
         </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        <Side>
+          <LaptopNav>
+            <NavLink href="/sale">SaleLonger</NavLink>
+            <NavLink href="/new">New&nbsp;ReleasesLonger</NavLink>
+            <NavLink href="/men">MenLonger</NavLink>
+            <NavLink href="/women">WomenLonger</NavLink>
+            <NavLink href="/kids">KidsLonger</NavLink>
+            <NavLink href="/collections">CollectionsLonger</NavLink>
+          </LaptopNav>
+          <MobileNav>
+            <UnstyledButton>
+              <Icon id="shopping-bag" strokeWidth={1} />
+            </UnstyledButton>
+            <UnstyledButton>
+              <Icon id="search" strokeWidth={1} />
+            </UnstyledButton>
+            <UnstyledButton onClick={() => setShowMobileMenu(true)}>
+              <Icon id="menu" strokeWidth={1} />
+            </UnstyledButton>
+          </MobileNav>
+        </Side>
       </MainHeader>
 
       <MobileMenu
@@ -46,12 +60,33 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: hidden;
+  overflow-x: auto;
+
+  @media ${QUERIES.phoneAndLower} {
+    padding: 18px 16px;
+  }
 `;
 
-const Nav = styled.nav`
+const LaptopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndLower} {
+    display: none;
+  }
+`;
+
+const MobileNav = styled.nav`
+  display: none;
+
+  @media ${QUERIES.tabletAndLower} {
+    display: flex;
+    justify-content: flex-end;
+    gap: 2rem;
+    margine: 0 2rem;
+  }
 `;
 
 const Side = styled.div`
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,37 +1,98 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Dialog aria-label="Main Menu">
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" strokeWidth={1} size={32} />
+        </CloseButton>
+        <MenuNav>
+          <a href="/sale">Sale</a>
+          <a href="/new">New&nbsp;Releases</a>
+          <a href="/men">Men</a>
+          <a href="/women">Women</a>
+          <a href="/kids">Kids</a>
+          <a href="/collections">Collections</a>
+        </MenuNav>
+        <MenuFooter>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </MenuFooter>
+      </Dialog>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: hsl(0deg 0% 0% / 0.5);
+`;
+
+const Dialog = styled(DialogContent)`
+  background-color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 330px;
+  padding: 32px 22px 32px 32px;
+  display: flex;
+  align-items: center;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 32px;
+  right: 22px;
+`;
+
+const MenuNav = styled.nav`
+  margin-top: auto;
+  margin-bottom: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const MenuFooter = styled.footer`
+  position: absolute;
+  display: flex;
+  gap: 14px;
+  flex-direction: column;
+  bottom: 32px;
+
+  a {
+    color: ${COLORS["gray"]["700"]};
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default MobileMenu;
